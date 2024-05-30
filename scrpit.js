7f2ffa4e59ca8d4951e62813c04edb3d166a6284
MisterBrash <matthew.brash@ocsb.ca>
2024-05-30 09:47:19 -0400

    Added the randInt function

---
+++ scrpit.js
@@ -0,0 +1,8 @@
+'use strict';
+
+randInt(min, max, inclusive = true) {
+  if (inclusive)
+    return Math.floor(Math.random() * (max - min + 1)) + min;  // included
+  else
+    return Math.floor(Math.random() * (max - min - 1)) + min + 1;  // not included
+}
