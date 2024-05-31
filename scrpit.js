ad6f9597f647f8fc986b39bf3e71bd9e4e8a8a2c
Cole Peterson <cole.peterson@stu.ocsb.ca>
2024-05-31 09:22:31 -0400

    pop

--- scrpit.js
+++ scrpit.js
@@ -6,3 +6,12 @@ randInt(min, max, inclusive = true) {
   else
     return Math.floor(Math.random() * (max - min - 1)) + min + 1;  // not included
 }
+
+let num1 = randInt(1,12)
+let num2 = randInt(1,12)
+
+console.log(result);
+
+
+
+
